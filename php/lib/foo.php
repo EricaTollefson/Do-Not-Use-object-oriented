c738aeae34d03965d2dbe6999b73d4d29c943446
Erica Tollefson <etollefson@cnm.edu>
2019-07-17 14:23:21 -0600

    updated foo.php with instantiation

---
+++ php/lib/foo.php
@@ -0,0 +1,13 @@
+/**
+* example:
+*$instance = new SimpleClass();
+*
+*// This can also be done with a variable:
+*$className = 'SimpleClass';
+*$instance = new $className(); // new SimpleClass()
+**/
+
+<?php
+$instance = new Author();
+
+?>
